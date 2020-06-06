fea8ce187c41a63633a82f6decb5a5b21d46cd89
EmptyException <joao_vitor9987@outlook.com.br>
2020-06-06 20:19:16 -0300

    Add avatar command

---
+++ src/commands/util/avatar.ts
@@ -0,0 +1,43 @@
+import { Command } from 'discord-akairo';
+import { Message, MessageEmbed, User } from 'discord.js';
+
+import colors from '~/config/colors';
+
+export default class AvatarCommand extends Command {
+  constructor() {
+    super('avatar', {
+      aliases: ['avatar'],
+      category: 'util',
+      clientPermissions: ['EMBED_LINKS'],
+      ratelimit: 2,
+      description: {
+        content: "Shows the user's avatar",
+        usage: '[user]',
+      },
+      args: [
+        {
+          id: 'user',
+          type: 'user',
+          default: (message: Message) => message.author,
+          description: 'User to fetch avatar',
+          prompt: {
+            start: 'What user would you like to get the avatar?\n',
+          },
+        },
+      ],
+    });
+  }
+
+  public exec(message: Message, { user }: { user: User }) {
+    const embed = new MessageEmbed()
+      .setColor(colors.success)
+      .setDescription(`Avatar for **${user.tag}**`)
+      .setImage(
+        user.displayAvatarURL({
+          format: 'png',
+          size: 1024,
+        })
+      );
+    return message.util!.send('', embed);
+  }
+}
