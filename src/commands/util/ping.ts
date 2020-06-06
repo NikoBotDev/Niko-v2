926aaa008890de0ad8dd1b3508d9f6e8481185f7
EmptyException <joao_vitor9987@outlook.com.br>
2020-06-06 19:58:52 -0300

    Add better ping command

--- src/commands/util/ping.ts
+++ src/commands/util/ping.ts
@@ -8,7 +8,12 @@ export default class Ping extends Command {
     });
   }
 
-  exec(message: Message) {
-    return message.reply('Pong!');
+  async exec(message: Message) {
+    const pingMsg = (await message.util!.send('Pinging~')) as Message;
+    const latency = pingMsg.createdTimestamp - message.createdTimestamp;
+    return message.util!.send([
+      `**Gateway Ping~ ${latency}ms**`,
+      `**API Ping~ ${Math.round(this.client.ws.ping)}ms**`,
+    ]);
   }
 }
