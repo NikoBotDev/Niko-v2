cab97c2419f16b6d29d40df700ede9dfa2097368
EmptyException <joao_vitor9987@outlook.com.br>
2020-06-07 20:54:52 -0300

    Add help and "command list" commands

---
+++ src/commands/help/commands.ts
@@ -0,0 +1,97 @@
+import { Command, Argument, PrefixSupplier } from 'discord-akairo';
+import { Message, MessageEmbed, PermissionResolvable } from 'discord.js';
+import { stripIndents, oneLine } from 'common-tags';
+import { dirname } from 'path';
+
+import colors from '~/config/colors';
+
+export default class CommandListCommand extends Command {
+  constructor() {
+    super('commands', {
+      aliases: ['commands', 'cmds'],
+      category: 'help',
+      description: {
+        content: oneLine`Shows the command list, pass no args to see the
+        list of available categories.`,
+        usage: '[category]',
+        examples: ['util', ''],
+      },
+      args: [
+        {
+          id: 'category',
+          prompt: {
+            start: async (message: Message) => {
+              const embed = this.getGroupList();
+              message.channel.send(
+                'What group you want to see the commands in?',
+                embed
+              );
+            },
+          },
+          type: Argument.validate(
+            'lowercase',
+            (_, category) => category !== 'owner'
+          ),
+          default: '',
+        },
+      ],
+    });
+  }
+
+  async exec(message: Message, { category }: { category: string }) {
+    const { modules } = this.handler;
+    const commands = modules.filter((mdl) => {
+      return (
+        !dirname(mdl.filepath).includes('subcommands') &&
+        this.isAllowed(mdl, category, message)
+      );
+    });
+
+    const getPrefix = this.handler.prefix as PrefixSupplier;
+
+    const embed = new MessageEmbed()
+      .setColor(colors.bot)
+      .setDescription('Command List')
+      .addField(
+        category,
+        commands.map((mdl) => `${getPrefix(message)}${mdl.id}`).join('\n'),
+        true
+      )
+      .setFooter('These are only the commands you are able to use.');
+
+    return message.channel.send('', embed);
+  }
+
+  getGroupList() {
+    const categories = this.handler.categories
+      .filter((cat) => cat.id !== 'owner')
+      .map((cat) => `[**${cat.id}**]`);
+
+    const embed = new MessageEmbed()
+      .setColor(colors.bot)
+      .setTitle('Group List')
+      .setDescription(stripIndents`${categories.join('\n')}`);
+    return embed;
+  }
+
+  isAllowed(mdl: Command, category: string, message: Message) {
+    if (
+      mdl.ownerOnly ||
+      (mdl.categoryID && mdl.categoryID !== category.toLowerCase())
+    ) {
+      return false;
+    }
+    if (mdl.channel === 'guild' && message.member) {
+      if (
+        mdl.userPermissions &&
+        message.member.permissions.missing(
+          mdl.userPermissions as PermissionResolvable[]
+        ).length !== 0
+      ) {
+        return false;
+      }
+      return true;
+    }
+    return true;
+  }
+}
---
+++ src/commands/help/help.ts
@@ -0,0 +1,79 @@
+import { Command, Argument, PrefixSupplier } from 'discord-akairo';
+import { Message, MessageEmbed } from 'discord.js';
+import { stringify } from 'querystring';
+
+import colors from '~/config/colors';
+
+export default class HelpCommand extends Command {
+  constructor() {
+    super('help', {
+      aliases: ['help', 'h'],
+      category: 'help',
+      description: {
+        content:
+          'Displays a list of available commands, or detailed information for a specified command.',
+        usage: '[command]',
+      },
+      ratelimit: 2,
+      clientPermissions: ['EMBED_LINKS'],
+      args: [
+        {
+          id: 'command',
+          description: 'Command to get info about',
+          type: Argument.union('command', 'commandAlias'),
+          prompt: {
+            optional: true,
+          },
+        },
+      ],
+    });
+  }
+
+  exec(message: Message, { command }: { command?: Command }) {
+    const getPrefix = this.handler.prefix as PrefixSupplier;
+    const prefix = getPrefix(message);
+    if (!command) {
+      const query = stringify({
+        client_id: this.client.user!.id,
+        permissions: 379904,
+        scope: 'bot',
+      });
+      const url = `https://discordapp.com/oauth2/authorize?${query}`;
+      const embed = new MessageEmbed()
+        .setColor(colors.success)
+        .setAuthor(message.author.username, message.author.displayAvatarURL())
+        .setDescription(
+          `Hello ${message.author.username}, i'm ${
+            this.client.user!.username
+          } here is some links that might help you, check them below!\nIf you have any questions or want to report a bug, please enter in the [Support Server](https://www.google.com.br/) you'll be very welcome in there! If you want to add me in your server click in the link below! ${url}\n`
+        )
+        .setFooter(
+          this.client.user!.username,
+          this.client.user!.displayAvatarURL()
+        );
+      return message.channel.send(embed);
+    }
+
+    const embed = new MessageEmbed()
+      .setColor(colors.bot)
+      .setTitle(
+        `\`${command.aliases[0]} ${
+          command.description.usage ? command.description.usage : ''
+        }\``
+      )
+      .addField('Description', command.description.content || '\u200b');
+
+    if (command.aliases.length > 1)
+      embed.addField('Aliases', `\`${command.aliases.join('` `')}\``, true);
+    if (command.description.examples && command.description.examples.length)
+      embed.addField(
+        'Examples',
+        `\`${prefix + command.aliases[0]} ${command.description.examples.join(
+          `\`\n\`${prefix + command.aliases[0]} `
+        )}\``,
+        true
+      );
+
+    return message.util!.send(embed);
+  }
+}
