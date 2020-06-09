73ab932d5dee3dbfdf14d4be6f208f6ef060afae
EmptyException <joao_vitor9987@outlook.com.br>
2020-06-08 21:22:48 -0300

    Add Tag Entity

---
+++ src/database/entities/Tag.ts
@@ -0,0 +1,37 @@
+// @ts-nocheck
+import {
+  Entity,
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryColumn,
+} from 'typeorm';
+import { Length } from 'class-validator';
+
+@Entity({
+  name: 'tags',
+})
+export class Tag extends BaseEntity {
+  @Column()
+  @PrimaryColumn()
+  @Length(3, 80, {
+    message: 'Tag name must be 3~80 characters long',
+  })
+  name: string;
+
+  @Column()
+  content: string;
+
+  @Column()
+  guildId: string;
+
+  @Column()
+  userId: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
