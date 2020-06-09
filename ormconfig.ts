b02eb51c10d384e14ac9e9b0772ec7167500c70d
EmptyException <joao_vitor9987@outlook.com.br>
2020-06-08 21:23:40 -0300

    fix: Wrong Entities folder in ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,7 +2,7 @@ export = {
   type: 'sqlite',
   migrations: ['./src/database/migrations/*.ts'],
   database: './.data/db.sqlite',
-  entities: ['./src/database/migrations/*.ts'],
+  entities: ['./src/database/entities/*.ts'],
   cli: {
     migrationsDir: './src/database/migrations',
     entitiesDir: './src/database/entities',
