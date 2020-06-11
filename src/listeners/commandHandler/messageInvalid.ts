9afff5ba10a2130834b5e1ca296a59c34c89ba9a
EmptyException <joao_vitor9987@outlook.com.br>
2020-06-11 12:01:49 -0300

    Add messageInvalid handler to handle tags

---
+++ src/listeners/commandHandler/messageInvalid.ts
@@ -0,0 +1,38 @@
+import { Listener, PrefixSupplier } from 'discord-akairo';
+import { Message } from 'discord.js';
+import { Tag } from '~/database/entities/Tag';
+export default class MessageInvalidListener extends Listener {
+  constructor() {
+    super('messageInvalid', {
+      emitter: 'commandHandler',
+      event: 'messageInvalid',
+    });
+  }
+
+  async exec(message: Message) {
+    const getPrefix = this.client.commandHandler.prefix as PrefixSupplier;
+
+    const prefix = getPrefix(message) as string;
+
+    if (!message.content.startsWith(prefix)) return;
+
+    const tag = message.content.slice(1).split(' ')[0];
+
+
+    const storedTag = await Tag.findOne({
+      where: {
+        name: tag
+      }
+    });
+
+    if (!storedTag) return;
+
+    await Tag.update({
+      name: tag
+    }, {
+      usages: storedTag.usages + 1
+    });
+
+    return message.channel.send(storedTag.content);
+  }
+}
