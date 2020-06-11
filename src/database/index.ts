ee7ebde1b40bded7bf5fd680fe0a3623c6e86691
EmptyException <joao_vitor9987@outlook.com.br>
2020-06-11 20:10:42 -0300

    Run pending migrations after connecting to the database

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,10 +1,11 @@
-import { createConnection, Connection } from 'typeorm';
+import { createConnection, Connection, MigrationExecutor } from 'typeorm';
 
 class Database {
   public connection?: Connection;
 
   async init() {
     this.connection = await createConnection();
+    await this.connection.runMigrations();
   }
 }
 
