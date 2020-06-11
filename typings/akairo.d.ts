67af6e9c06eb4988ab9045496ba6d39c9d02cc00
EmptyException <joao_vitor9987@outlook.com.br>
2020-06-11 12:00:13 -0300

    Add settings property to AkairoClient

--- typings/akairo.d.ts
+++ typings/akairo.d.ts
@@ -1,8 +1,10 @@
 import { CommandHandler } from 'discord-akairo';
 import Niko from './classes/Niko';
+import TypeORMProvider from '~/database/providers/TypeORMProvider';
 
 declare module 'discord-akairo' {
   interface AkairoClient {
     commandHandler: CommandHandler;
+    settings: TypeORMProvider;
   }
 }
