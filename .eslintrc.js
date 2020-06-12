e92cb530f6130ab814354df95cb241e6683dfe10
EmptyException <joao_vitor9987@outlook.com.br>
2020-06-12 11:51:27 -0300

    Add prettier typescript plugin to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,10 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
-    'plugin:prettier/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
