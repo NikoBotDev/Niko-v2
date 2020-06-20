c9b11ed878c299dd5f2509351f47856eb08cac71
EmptyException <joao_vitor9987@outlook.com.br>
2020-06-20 15:54:45 -0300

    Add profiles table migration

---
+++ src/database/migrations/1592240920413-create_profiles_table.ts
@@ -0,0 +1,65 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createProfilesTable1592240920413 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'profiles',
+        columns: [
+          {
+            name: 'userId',
+            type: 'text',
+            isPrimary: true,
+          },
+          {
+            name: 'level',
+            type: 'int',
+            default: 1,
+          },
+          {
+            name: 'xp',
+            type: 'int',
+            default: 1,
+          },
+          {
+            name: 'coins',
+            type: 'int',
+            default: 0,
+          },
+          {
+            name: 'married',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'profile_bg',
+            type: 'varchar',
+            default: "'default'",
+          },
+          {
+            name: 'badges',
+            type: 'text',
+            default: "'[]'",
+          },
+          {
+            name: 'streak',
+            type: 'int',
+            default: 0,
+          },
+          {
+            name: 'createdAt',
+            type: 'datetime',
+          },
+          {
+            name: 'updatedAt',
+            type: 'datetime',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('profiles');
+  }
+}
