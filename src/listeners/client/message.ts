8719d90691bbae69f1bff79597d14441d080fd67
EmptyException <joao_vitor9987@outlook.com.br>
2020-06-22 15:10:34 -0300

    Base levelling system

--- src/listeners/client/message.ts
+++ src/listeners/client/message.ts
@@ -1,4 +1,7 @@
 import { Listener } from 'discord-akairo';
+import { Message } from 'discord.js';
+import { findOrMake } from '~/util/profile';
+import { addRewards, toNextLevel } from '~/util/xp';
 
 export default class MessageListener extends Listener {
   constructor() {
@@ -8,7 +11,23 @@ export default class MessageListener extends Listener {
     });
   }
 
-  exec() {
-    console.log('Message received');
+  async exec(message: Message) {
+    if (message.author.bot) return;
+
+    const user = await findOrMake({
+      userId: message.author.id,
+    });
+
+    if (!user) return;
+
+    addRewards(user);
+
+    const nextLevelXp = toNextLevel(user.level, true) as number;
+    if (user.xp >= nextLevelXp) {
+      user.level += 1;
+      user.xp -= nextLevelXp;
+    }
+
+    await user.save();
   }
 }
